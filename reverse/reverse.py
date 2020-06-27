53c4f84e1c703141c43cf7c1f6f23302b4a2e8b8
Clif Hodges <clifhodges13@gmail.com>
2020-06-27 13:31:26 -0500

    reverse reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -16,6 +16,14 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __str__(self):
+        output = ''
+        current_node = self.head
+        while current_node is not None:
+            output += f'{current_node.value} > '
+            current_node = current_node.next_node
+        return output + 'None'
+
     def add_to_head(self, value):
         node = Node(value)
 
@@ -39,4 +47,12 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # we'll need to reference the previous node
+        # traverse the list and when we get to the end, that node will become the new head
+        current = self.head 
+        while current is not None: 
+            self.next_node = current.next_node
+            current.next_node = prev 
+            prev = current 
+            current = self.next_node
+        self.head = prev
